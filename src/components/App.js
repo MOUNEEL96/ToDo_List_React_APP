d924496111191af705117b2bd9e390f93f75ffaa
Mouneel <mouneelsmailbox@gmail.com>
2022-08-30 21:31:58 +0530

    make it better

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./../styles/App.css";
+import Todolist from "./Todolist";
+
+function App() 
+{
+	return (
+	<div className="to-do-app">
+	
+	<Todolist/>
+	</div>
+	);
+}
+
+
+export default App;
